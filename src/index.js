6b28e015524d932230c6e5462b75656f24ad8a3d
Evgeny <s-a-w-13@mail.ru>
2020-09-30 19:31:38 +0300

    feat:added solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array==null || array=='') return 0;
+    array=array.sort(function(a, b) { return a - b; });
+    return array[0];
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array==null || array=='') return 0;
+  array=array.sort(function(a, b) { return a-b; });
+  array.reverse();
+    return array[0] ;
 }
 
+
 exports.avg = function avg (array) {
-  return 0;
+  if (array==null || array=='') return 0;
+  let sum=0;
+  array.forEach(element => {
+    sum+=element;
+  });
+  return (sum/array.length)
 }
+
